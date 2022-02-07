b31808b77614f6622224fc8038b66262a340b854
Patrick <prm5983@rit.edu>
2022-02-07 12:42:17 -0500

    Velocity averaging

--- Starchaeologist/Assets/Scripts/Player/PlayerWhip.cs
+++ Starchaeologist/Assets/Scripts/Player/PlayerWhip.cs
@@ -4,12 +4,61 @@ using UnityEngine;
 
 public class PlayerWhip : MonoBehaviour
 {
-    //Things needed:
-    //Current pos
-    //Previous pso
-    //From those, get current estimated velocity; (pos - prev) / Time.deltaTime
-    //store that calculated velocity in a queue with a set size, so we have multiple
-    //  velocities to average between across the past few frames
-    //Use averaged velocity to determine where the player's hand is going, and make the whip go there
-    //Remember to account for division by 0, like if Time.timeScale becomes 0 due to a pause or something
+    /*TODO:
+     * Only update hand velocity if holding button? Move velocity calculation to a new script independent of whip?
+     * While holding button, if velocity matches or exceeds minimum swing speed, extend whip
+     * Make extended whip detect if it hit a yoinkable thing, and engage the yoink if so
+    */
+
+    [Tooltip("The max number of elements allowed in the velocity bank. " +
+        "The bank will very exceed the capacity by one for ~1 line of code before it can be pruned down.")]
+    [SerializeField] private int velBankCapacity = 10;
+
+    private Vector3 previousPos;
+    private LinkedList<Vector3> estVelocityBank;
+    public Vector3 CurrentAvgVelocity { get; private set; }
+
+    private void Start()
+    {
+        estVelocityBank = new LinkedList<Vector3>();
+        previousPos = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateVelocityBank();
+        CurrentAvgVelocity = GetAverageFromBank();
+    }
+
+    /// <summary>
+    /// Estimates the current velocity, adds it to <see cref="estVelocityBank"/>, and removes the oldest estimate 
+    /// if the bank exceeds <see cref="velBankCapacity"/>.<br/>
+    /// This method does nothing if <see cref="Time.fixedDeltaTime"/> is a near-zero float.
+    /// </summary>
+    private void UpdateVelocityBank()
+    {
+        if (Mathf.Approximately(Time.fixedDeltaTime, 0))
+        {
+            Vector3 vel = (transform.position - previousPos) / Time.fixedDeltaTime;
+            estVelocityBank.AddFirst(vel);
+            if (estVelocityBank.Count > velBankCapacity)
+            {
+                estVelocityBank.RemoveLast();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Loops through <see cref="estVelocityBank"/> to return the mean average of its elements.
+    /// </summary>
+    /// <returns>The mean average of <see cref="estVelocityBank"/>.</returns>
+    private Vector3 GetAverageFromBank()
+    {
+        Vector3 avg = Vector3.zero;
+        foreach (Vector3 vel in estVelocityBank)
+        {
+            avg += vel;
+        }
+        return avg / estVelocityBank.Count;
+    }
 }
