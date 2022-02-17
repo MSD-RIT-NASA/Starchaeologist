435bec1bd5d37ade5cc79b2a05739b957aa4959e
nickvisc956 <nickvisc956@aol.com>
2022-02-17 17:11:59 -0500

    adjusting raft to listen to python script

--- Starchaeologist/Assets/Scripts/Platforms/S_Raft.cs
+++ Starchaeologist/Assets/Scripts/Platforms/S_Raft.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class S_Raft : MonoBehaviour
 {
-    public float tiltRange = 10f;
+    public float maxRange = 10f;
+    public float tiltRange = 0f;
     public float tiltSpeed = 1.0f;
     public bool tilting = false;
+    public Vector3 plannedRotation;
 
     Quaternion newTilt;
     Quaternion oldTilt;
@@ -17,32 +19,43 @@ public class S_Raft : MonoBehaviour
     {
         newTilt = Quaternion.Euler(0, 0, 0);
         oldTilt = Quaternion.Euler(0, 0, 0);
+
+        plannedRotation = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        tiltRatio = Mathf.Clamp(tiltRatio, 0f, 1f);
-        Raft();
+        //Raft();
     }
 
     //Raft() simulates the motion of a raft on water by randomly choosing rotations and lerping to those rotations
     public void Raft()
     {
+        tiltRatio = Mathf.Clamp(tiltRatio, 0f, 1f);
+
         //check if the new rotation has been reached
         if (transform.localRotation == newTilt)
         {
-            //choose a rotation to lerp to
-            newTilt = Quaternion.Euler(Random.Range(-tiltRange, tiltRange), 0, Random.Range(-tiltRange, tiltRange));
+            if(tilting)
+            {
+                //choose a rotation to lerp to
+                newTilt = Quaternion.Euler(Random.Range(-tiltRange, tiltRange), 0, Random.Range(-tiltRange, tiltRange));
+            }
+            else
+            {
+                //rotate back to zero
+                newTilt = Quaternion.Euler(0, 0, 0);
+            }
+
             oldTilt = transform.localRotation;
 
-            //choose a random speed to lerp at and reset the timer
-            //tiltSpeed = Random.Range(0.75f, 2.0f);
             tiltRatio = 0.0f;
         }
 
         //lerp
-        transform.localRotation = Quaternion.Slerp(oldTilt, newTilt, tiltRatio);
+        //transform.localRotation = Quaternion.Slerp(oldTilt, newTilt, tiltRatio);
+        plannedRotation = transform.localEulerAngles;
 
         //increment time
         tiltRatio = tiltRatio + Time.deltaTime * tiltSpeed;
