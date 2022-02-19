342efc41218a8ff30a305118be4ac75081042c60
William Johnson <wcj7833@rit.edu>
2022-02-18 20:25:24 -0500

    Removed thread usage

--- External/Python/server.py
+++ External/Python/server.py
@@ -7,17 +7,16 @@ from pip import main
 # import serial
 import time
 from _thread import *;
-from threading import Thread, Lock;
 import logging
 import zmq
 
-class ServerThread(Thread):
+class Server():
     def __init__(self, port):
         # self.arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)    
         context = zmq.Context()
         self.socket = context.socket(zmq.REP)
         self.socket.bind(port)
-        self.lock = Lock()
+        # self.lock = Lock()
         self.score = None;
 
     def calculateBalanceScore(self,sensorData):
@@ -30,41 +29,38 @@ class ServerThread(Thread):
     # def arduinoRead(self):
     #    data = self.arduino.readline()
     #    return data
-
-    def run(self):
-        while True :
-            # Recieve messages from the Unity Game
-            message = self.socket.recv()
-            decodedMessage = message.decode("utf-8")
-            if(decodedMessage == "endGame"):
-                # TODO: Send game score to unity
-                print("End of game reached")
-            elif(decodedMessage == "closeGame"):
-                # TODO: Send game score to unity
-                print("End of application reached")
-                break
-            elif(decodedMessage == "readScore"):
-                # TODO: Read values from database
-                print("Data recieved")
-                # Send message that data has been recieved so game can start again
-                
-                # Put array in algorithm 
-                self.score = self.calculateBalanceScore()
-
-            else:
-                posList = decodedMessage.split(" ")
-                # TODO: Set Motion Floor Platform to these angles
-
-                # Send angles back to Unity Game as confirmation
-                unityAngles = str(posList[0]) + " " + str(posList[1]) 
-                encodedMessage = unityAngles.encode()
-                self.socket.send(encodedMessage)
-    
-        
     
 
 if __name__ == "__main__":
     port = "tcp://*:5555"
-    server = ServerThread(port)
-    server.start()
-    server.join()
+    server = Server(port)
+    score = None
+    while True :
+        # Recieve messages from the Unity Game
+        message = server.socket.recv()
+        decodedMessage = message.decode("utf-8")
+        if(decodedMessage == "endGame"):
+            # TODO: Send game score to unity
+            print("End of game reached")
+            score = None
+        elif(decodedMessage == "closeGame"):
+            # TODO: Send game score to unity
+            print("End of application reached")
+            break
+        elif(decodedMessage == "readScore"):
+            # TODO: Read values from database
+            print("Data recieved")
+            # Send message that data has been recieved so game can start again
+            
+            # Put array in algorithm 
+            score = server.calculateBalanceScore()
+
+        else:
+            posList = decodedMessage.split(" ")
+            # TODO: Set Motion Floor Platform to these angles
+
+            # Send angles back to Unity Game as confirmation
+            unityAngles = str(posList[0]) + " " + str(posList[1]) 
+            encodedMessage = unityAngles.encode()
+            server.socket.send(encodedMessage)
+    
