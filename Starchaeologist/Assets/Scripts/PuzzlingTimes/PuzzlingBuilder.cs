8713768efcacb77339351040df619f6d10d86e59
nickvisc956 <nickvisc956@aol.com>
2022-02-23 23:12:44 -0500

    modified puzzling times

--- Starchaeologist/Assets/Scripts/PuzzlingTimes/PuzzlingBuilder.cs
+++ Starchaeologist/Assets/Scripts/PuzzlingTimes/PuzzlingBuilder.cs
@@ -6,8 +6,12 @@ public class PuzzlingBuilder : MonoBehaviour
 {
     public GameObject tilePrefab;
     List<GameObject>[] tileArray;
+    public List<bool>[] trapArray;
 
-    int roomLength = 10;
+    public GameObject perimeterPrefab;
+    List<GameObject> perimeterList = new List<GameObject>();
+
+    public int roomLength = 4;
 
     // Start is called before the first frame update
     void Awake()
@@ -20,17 +24,45 @@ public class PuzzlingBuilder : MonoBehaviour
     {
         //instantiate the tile array
         tileArray = new List<GameObject>[6];
+        trapArray = new List<bool>[6];
         for (int i = 0; i < tileArray.Length; i++)
         {
             tileArray[i] = new List<GameObject>();
-            for (int j = 0; j < roomLength; j++)
+            trapArray[i] = new List<bool>();
+            for (int j = 0; j < (roomLength * 3); j++)
             {
                 //place the room tiles
                 tileArray[i].Add(null);
                 tileArray[i][j] = Instantiate(tilePrefab);
                 tileArray[i][j].transform.rotation = Quaternion.Euler(0, 90 * Random.Range(0, 4), 0);
                 tileArray[i][j].transform.position = new Vector3((i * 2) + 1, 0, (j * 2) + 1);
+                trapArray[i].Add(false);
+            }
+
+            //place trap platforms
+            int k = 0;
+            while(k < roomLength * 1.5)
+            {
+                int kIndex = Random.Range(0, trapArray[i].Count);
+                if(trapArray[i][kIndex])
+                {
+                    continue;
+                }
+                trapArray[i][kIndex] = true;
+                k++;
             }
         }
+
+        //place the outer walls of the room
+        for (int i = 0; i < roomLength; i++)
+        {
+            perimeterList.Add(null);
+            perimeterList[i] = Instantiate(perimeterPrefab);
+            int zPosition = (((i * 3) + 1) * 2) + 1;
+            perimeterList[i].transform.position = new Vector3(6, 0, zPosition);
+        }
+
+        //move the last platform to the end of the room
+        GameObject.Find("EndPlatform").transform.position = new Vector3(6, 0, roomLength * 6);
     }
 }
