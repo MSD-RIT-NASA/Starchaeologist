d692a491a1add19a7688375fba8501191f02dc00
npf9700 <70346282+npf9700@users.noreply.github.com>
2022-11-16 15:53:21 -0500

    Basic Indicator and Rotation
    A basic text UI element was added as a placeholder indicator and is supposed to rotate to point to the activated trap

--- Starchaeologist/Assets/Scripts/PuzzlingTimes/Trap_Indicator.cs
+++ Starchaeologist/Assets/Scripts/PuzzlingTimes/Trap_Indicator.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Trap_Indicator : MonoBehaviour
 {
@@ -12,17 +13,36 @@ public class Trap_Indicator : MonoBehaviour
     //Calculated by the difference of the player and target positions
     private Vector3 indicatingTarget;
 
+    public Text indicator;
+
+    //Calculated using indicatingTarget
+    private float angleToRotate;
+
     // Start is called before the first frame update
     void Start()
     {
         isActive = false;
         indicatingTarget = Vector3.zero;
+        angleToRotate = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        indicator.enabled = isActive;
+
+        if (isActive)
+        {
+            /* Pseudo-code
+                theta = angle between indicatingTarget and indicator.right
+                indicator.rotate by theta degrees
+
+                cos(theta) = x-axis/indicatingTarget
+                cos(theta) = indicatingTarget.x/Math.sqrt(indicatingTarget.x^2 + indicatingTarget.y^2)
+            */
+            angleToRotate = Mathf.Acos(indicatingTarget.x / Mathf.Sqrt(Mathf.Pow(indicatingTarget.x, 2) + Mathf.Pow(indicatingTarget.y, 2)));
+            indicator.transform.Rotate(new Vector3(0.0f, 0.0f, angleToRotate));
+        }
     }
 
     //Sets the target transform and calculate the indicating vector accordingly
