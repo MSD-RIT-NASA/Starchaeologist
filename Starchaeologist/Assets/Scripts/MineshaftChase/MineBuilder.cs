2891edf8e0f87ca269476c0b66c0ec4a2346d859
dbg9180 <70482975+dbg9180@users.noreply.github.com>
2023-01-18 19:10:19 -0500

    Minecart level scripts
    Segments will load but obstacles create an error. Need to make separate script to move forward to avoid messing with river level.

--- Starchaeologist/Assets/Scripts/MineshaftChase/MineBuilder.cs
+++ Starchaeologist/Assets/Scripts/MineshaftChase/MineBuilder.cs
@@ -6,14 +6,22 @@ public class MineBuilder : MonoBehaviour
 {
     public int segmentCount = 10;
 
+    List<GameObject> spawnedSegments = new List<GameObject>();
+
+    public List<GameObject> segmentPrefabs_2M = new List<GameObject>();
     public List<GameObject> obstaclePrefabs = new List<GameObject>();
 
-    List<List<Vector3>> obstacleSpawns; 
+    List<GameObject>[] segmentArray;
+
+    List<List<Vector3>> obstacleSpawns;
 
     // Start is called before the first frame update
     void Start()
     {
         DataSetup();
+        SegmentSetup();
+
+        GetComponent<MineGame>().trackReferences = spawnedSegments;
         //remove the script 
         Destroy(this);
     }
@@ -27,6 +35,94 @@ public class MineBuilder : MonoBehaviour
     private void DataSetup()
     {
         obstacleSpawns = new List<List<Vector3>>(segmentCount);
+        segmentArray = new List<GameObject>[1];
+        segmentArray[0] = segmentPrefabs_2M;
+    }
 
+    private void SegmentSetup()
+    {
+        //spawn the amount of track requested
+        spawnedSegments.Add(GameObject.Find("Plane"));
+
+        Vector3 spawnPosition = new Vector3(0, 0, 100);
+        int i = 0;
+        while (i < segmentCount)
+        {
+            //choose one of the available segment prefabs and place it at the end of the last placed piece
+            GameObject newSpawn = Instantiate(segmentArray[0][Random.Range(0, segmentArray[0].Count)]);
+            newSpawn.transform.position = spawnPosition;
+
+            if (i >= 5)
+            {
+                newSpawn.SetActive(false);
+            }
+            spawnPosition = new Vector3(0,0,newSpawn.transform.position.z+100);
+
+            spawnedSegments.Add(newSpawn);
+
+            //record the positions available for spawning obstacles and artifacts
+            obstacleSpawns.Add(new List<Vector3>());
+            obstacleSpawns[i].Add(newSpawn.transform.position);
+            int j = 2;
+            while (j < newSpawn.transform.childCount)
+            {
+                obstacleSpawns[i].Add(newSpawn.transform.position);
+                j++;
+            }
+
+            i++;
+        }
+
+        //spawn artifact pieces, treasure, and obstacles along the river
+        //i = 0;
+        //while (i < segmentCount * 2)
+        //{
+        //    ////artifacts spawner
+        //    //if (i < artifactPieces)
+        //    //{
+        //    //    //do this once there are actual pieces. depending on the artifact, this will change based on the amount of pieces
+        //    //    //newSpawn = Instantiate(artifactPrefabs[i]);
+        //    //    PlaceThings(artifactPrefabs[0]);
+        //    //}
+
+        //    //treasure spawner
+        //   // PlaceThings(treasurePrefabs[Random.Range(0, treasurePrefabs.Count)]);
+
+        //    //obstacle spawner
+        //    PlaceThings(obstaclePrefabs[Random.Range(0, obstaclePrefabs.Count)]);
+
+
+        //    i++;
+        //}
+
+    }
+
+    //choose a location from the list to place the item
+    private void PlaceThings(GameObject spawnThis)
+    {
+        //see if there is a location to spawn at
+        Vector3 givePosition = Vector3.zero;
+        int i = -1;
+        while (givePosition == Vector3.zero)
+        {
+            //choose a river segment to spawn on
+            i = Random.Range(0, obstacleSpawns.Count);
+            if (obstacleSpawns[i].Count != 0)
+            {
+                //choose a checkpoint on said river to spawn at
+                int j = 0; //Random.Range(0, obstacleSpawns[i].Count);
+                givePosition = obstacleSpawns[i][j];
+                obstacleSpawns[i].RemoveAt(j);
+            }
+            else
+            {
+                obstacleSpawns.RemoveAt(i);
+            }
+        }
+
+        i++;
+        GameObject newSpawn = Instantiate(spawnThis, spawnedSegments[i].transform);
+        newSpawn.transform.position = givePosition;
+        newSpawn.transform.rotation = Quaternion.Euler(0, (Random.Range(0, 2) * 180), 0);
     }
 }
--- Starchaeologist/Assets/Scripts/MineshaftChase/MineGame.cs
+++ Starchaeologist/Assets/Scripts/MineshaftChase/MineGame.cs
@@ -4,15 +4,123 @@ using UnityEngine;
 
 public class MineGame : MonoBehaviour
 {
+    //singleton
+    public static MineGame singleton;
+
+    [SerializeField] GameObject playerReference;
+    [SerializeField] GameObject raftReference;
+    S_Raft raftScript;
+
+    public List<GameObject> trackReferences = new List<GameObject>(); //populated with positions while the river is being built from the S_RiverBuilder script
+    Vector3 nextDestination = new Vector3(0, 0, 0);
+    Vector3 currentDirection = new Vector3(0, 0, 1);
+    public float cartAcceleration = 0.1f;
+    public float cartSpeed = 3.0f;
+    float currentSpeed = 0f;
+    public bool timeToMove = true; //turned true the first time the player teleports to the raft from the S_RaftCollision script
+    bool slowDown = false;
+    bool playerAttached = false;
+    int checkpointIndex = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (singleton != null && singleton != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            singleton = this;
+        }
+
+        raftScript = raftReference.transform.GetChild(1).GetComponent<S_Raft>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //start the game by moving the raft
+        if (timeToMove)
+        {
+            //stick the player under the raft gameobject to help with movement
+            if (!playerAttached)
+            {
+                raftScript.tilting = true;
+                playerAttached = true;
+                playerReference.transform.parent = raftReference.transform;
+                playerReference.transform.position = raftReference.transform.GetChild(0).position;
+            }
+            MoveRaft();
+            //make the raft rotate
+            raftScript.Raft();
+        }
     }
+    void MoveRaft()
+    {
+
+        //accelerate or decelerate the raft
+        if (currentSpeed != cartSpeed && !slowDown)
+        {
+            currentSpeed = currentSpeed + (cartAcceleration * Time.deltaTime * cartSpeed * 5f);
+
+            raftScript.tiltRange = raftScript.tiltRange + (cartAcceleration * Time.deltaTime * raftScript.maxRange * 5f);
+        }
+        else if (slowDown)
+        {
+            currentSpeed = currentSpeed - (cartAcceleration * Time.deltaTime * cartSpeed);
+
+            raftScript.tiltRange = raftScript.tiltRange - (cartAcceleration * Time.deltaTime * raftScript.maxRange);
+        }
+        currentSpeed = Mathf.Clamp(currentSpeed, 0.25f, cartSpeed);
+        raftScript.tiltRange = Mathf.Clamp(raftScript.tiltRange, 0.25f, raftScript.maxRange);
+
+        //Direct the raft to the next checkpoint
+        Vector3 desiredDirection = Vector3.Normalize(nextDestination - raftReference.transform.position);
+        if (Mathf.Abs(Vector3.Angle(desiredDirection, currentDirection)) < 1f)
+        {
+            currentDirection = desiredDirection;
+        }
+        else
+        {
+            currentDirection = Vector3.Normalize(currentDirection + (desiredDirection * Time.deltaTime * currentSpeed));
+        }
+
+        //move the raft
+        raftReference.transform.position += currentDirection * Time.deltaTime * currentSpeed;
+
+        //check if the raft has reach the checkpoint then go to the next one
+        if (Vector3.Distance(raftReference.transform.position, nextDestination) < 1f)
+        {
+            checkpointIndex++;
+
+            if (checkpointIndex == trackReferences.Count - 1)//if this is the last checkpoint to go to, start slowing down the raft
+            {
+                slowDown = true;
+                nextDestination = trackReferences[checkpointIndex].transform.GetChild(1).transform.position;
+            }
+            else if (checkpointIndex == trackReferences.Count)//if the raft has reached the last checkpoint, stop
+            {
+                slowDown = false;
+                timeToMove = false;
+                playerAttached = false;
+                playerReference.transform.parent = null;
+            }
+            else//otherwise set the new destination tot he next checkpoint
+            {
+                nextDestination = trackReferences[checkpointIndex].transform.GetChild(1).transform.position;
+            }
+
+            //optimization
+            if (checkpointIndex - 5 >= 0)//disable river segments that are far behind the player
+            {
+                trackReferences[checkpointIndex - 5].SetActive(false);
+            }
+            if (checkpointIndex + 5 < trackReferences.Count)//enable segments that are getting close to the player
+            {
+                trackReferences[checkpointIndex + 5].SetActive(true);
+            }
+        }
+    }
+
 }
