0ad1f38aee3b688d791fc3881ec2e0f44f9d5c81
dbg9180 <70482975+dbg9180@users.noreply.github.com>
2023-01-19 10:31:29 -0500

    Minecart gray boxing
    obstacles instantiate, need randomization

--- Starchaeologist/Assets/Scripts/MineshaftChase/MineBuilder.cs
+++ Starchaeologist/Assets/Scripts/MineshaftChase/MineBuilder.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MineBuilder : MonoBehaviour
 {
-    public int segmentCount = 10;
+    public int segmentCount = 5;
 
     List<GameObject> spawnedSegments = new List<GameObject>();
 
@@ -44,7 +44,7 @@ public class MineBuilder : MonoBehaviour
         //spawn the amount of track requested
         spawnedSegments.Add(GameObject.Find("Plane"));
 
-        Vector3 spawnPosition = new Vector3(0, 0, 100);
+        Vector3 spawnPosition = new Vector3(0, -1, 5);
         int i = 0;
         while (i < segmentCount)
         {
@@ -56,17 +56,17 @@ public class MineBuilder : MonoBehaviour
             {
                 newSpawn.SetActive(false);
             }
-            spawnPosition = new Vector3(0,0,newSpawn.transform.position.z+100);
+            spawnPosition = new Vector3(0,-1,newSpawn.transform.position.z+10);
 
             spawnedSegments.Add(newSpawn);
 
             //record the positions available for spawning obstacles and artifacts
             obstacleSpawns.Add(new List<Vector3>());
-            obstacleSpawns[i].Add(newSpawn.transform.position);
+            obstacleSpawns[i].Add(new Vector3(0, 0, newSpawn.transform.position.z));
             int j = 2;
             while (j < newSpawn.transform.childCount)
             {
-                obstacleSpawns[i].Add(newSpawn.transform.position);
+                obstacleSpawns[i].Add(new Vector3(0, 0, newSpawn.transform.position.z));
                 j++;
             }
 
@@ -74,26 +74,26 @@ public class MineBuilder : MonoBehaviour
         }
 
         //spawn artifact pieces, treasure, and obstacles along the river
-        //i = 0;
-        //while (i < segmentCount * 2)
-        //{
-        //    ////artifacts spawner
-        //    //if (i < artifactPieces)
-        //    //{
-        //    //    //do this once there are actual pieces. depending on the artifact, this will change based on the amount of pieces
-        //    //    //newSpawn = Instantiate(artifactPrefabs[i]);
-        //    //    PlaceThings(artifactPrefabs[0]);
-        //    //}
+        i = 0;
+        while (i < segmentCount)
+        {
+            ////artifacts spawner
+            //if (i < artifactPieces)
+            //{
+            //    //do this once there are actual pieces. depending on the artifact, this will change based on the amount of pieces
+            //    //newSpawn = Instantiate(artifactPrefabs[i]);
+            //    PlaceThings(artifactPrefabs[0]);
+            //}
 
-        //    //treasure spawner
-        //   // PlaceThings(treasurePrefabs[Random.Range(0, treasurePrefabs.Count)]);
+            //treasure spawner
+            // PlaceThings(treasurePrefabs[Random.Range(0, treasurePrefabs.Count)]);
 
-        //    //obstacle spawner
-        //    PlaceThings(obstaclePrefabs[Random.Range(0, obstaclePrefabs.Count)]);
+            //obstacle spawner
+            PlaceThings(obstaclePrefabs[Random.Range(0, obstaclePrefabs.Count)]);
 
 
-        //    i++;
-        //}
+            i++;
+        }
 
     }
 
@@ -107,17 +107,21 @@ public class MineBuilder : MonoBehaviour
         {
             //choose a river segment to spawn on
             i = Random.Range(0, obstacleSpawns.Count);
+            Debug.Log(obstacleSpawns.Count);
             if (obstacleSpawns[i].Count != 0)
             {
                 //choose a checkpoint on said river to spawn at
                 int j = 0; //Random.Range(0, obstacleSpawns[i].Count);
                 givePosition = obstacleSpawns[i][j];
+                Debug.Log("check point at " + obstacleSpawns[i] + ", " + obstacleSpawns[i][j]);
                 obstacleSpawns[i].RemoveAt(j);
             }
             else
             {
+                Debug.Log("Removed " + obstacleSpawns[i]);
                 obstacleSpawns.RemoveAt(i);
             }
+            
         }
 
         i++;
