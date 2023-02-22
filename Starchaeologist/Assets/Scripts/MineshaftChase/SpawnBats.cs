388ac771020755db22142733f8680eb3542abdc9
npf9700 <70346282+npf9700@users.noreply.github.com>
2023-02-22 13:49:50 -0500

    BatSpawner Finished
    The bat spawner sends several animated bats from its position towards the player

--- Starchaeologist/Assets/Scripts/MineshaftChase/SpawnBats.cs
+++ Starchaeologist/Assets/Scripts/MineshaftChase/SpawnBats.cs
@@ -7,7 +7,11 @@ public class SpawnBats : MonoBehaviour
     [SerializeField]
     private GameObject batPrefab;
 
+    [SerializeField]
+    private List<GameObject> spawnPoints;
+
     private int numBats;
+    private float batSpeed;
 
     private List<GameObject> bats;
 
@@ -15,14 +19,18 @@ public class SpawnBats : MonoBehaviour
     void Start()
     {
         numBats = 0;
+        batSpeed = 0.05f;
         bats = new List<GameObject>();
-        Invoke("BatSpawn", 0.2f);
+        InvokeRepeating("BatSpawn", 0.0f, 0.3f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Get bats to move next
+        for(int i = 0; i < numBats; i++)
+        {
+            bats[i].transform.position += bats[i].transform.up * batSpeed;
+        }
     }
 
 
@@ -34,14 +42,17 @@ public class SpawnBats : MonoBehaviour
 
     public void BatSpawn()
     {
-        bats.Add(Instantiate(batPrefab, this.transform));
-        bats[numBats].transform.Rotate(0.0f, 5f * numBats, 0.0f, Space.Self);
+        //Spawn bat at a random spawnpoint chosen from the list
+        int randIndex = (int)Random.Range(0.0f, 5.0f);
+        bats.Add(Instantiate(batPrefab, spawnPoints[randIndex].transform));
+
+        //
+        //bats[numBats].transform.Rotate((5f * numBats), 0.0f, 0.0f, Space.Self);
         numBats++;
 
-        if(numBats > 5)
+        if(numBats > 10)
         {
             CancelInvoke("BatSpawn");
-            bats.Clear();
         }
     }
 }
