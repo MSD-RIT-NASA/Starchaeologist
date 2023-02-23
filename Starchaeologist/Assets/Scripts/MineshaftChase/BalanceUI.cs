c1b1cd723450989027723e6973b00dcce3dd0d46
npf9700 <70346282+npf9700@users.noreply.github.com>
2023-02-23 10:59:08 -0500

    UI Progress Indicator
    A UI screen now displays the minecart's rotation in relation to how it should ideally be oriented in the bottom left of the player's view

---
+++ Starchaeologist/Assets/Scripts/MineshaftChase/BalanceUI.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BalanceUI : MonoBehaviour
+{
+    [SerializeField]
+    private Minecart cart;
+
+    [SerializeField]
+    private Image balanceBar;
+    [SerializeField]
+    private Image safeZone;
+
+    private float balanceRot;
+
+    /*
+     * [SerializeField]
+     * private SensorData sens1;
+     * [SerializeField]
+     * private SensorData sens2;
+    */
+    // Start is called before the first frame update
+    void Start()
+    {
+        balanceRot = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        balanceRot = cart.transform.eulerAngles.z;
+        balanceBar.transform.eulerAngles = new Vector3(
+            balanceBar.transform.eulerAngles.x,
+            balanceBar.transform.eulerAngles.y,
+            balanceRot
+            );
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "StraightTrack")
+        {
+            safeZone.transform.eulerAngles = new Vector3(
+                safeZone.transform.eulerAngles.x,
+                safeZone.transform.eulerAngles.y,
+                0f);
+        }
+        else if (other.gameObject.tag == "RightTrack")
+        {
+            safeZone.transform.eulerAngles = new Vector3(
+                safeZone.transform.eulerAngles.x,
+                safeZone.transform.eulerAngles.y,
+                -10f);
+        }
+        else if (other.gameObject.tag == "LeftTrack")
+        {
+            safeZone.transform.eulerAngles = new Vector3(
+                safeZone.transform.eulerAngles.x,
+                safeZone.transform.eulerAngles.y,
+                10f);
+        }
+    }
+}
