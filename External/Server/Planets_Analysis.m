62c889ea33d5c5bdab52b18f2d01c54589c6c892
DanielJ <109160446+TheBostonGoblin@users.noreply.github.com>
2023-04-01 15:31:15 -0400

    Game can now be run multiple times and will not error without Slime

--- External/Server/Planets_Analysis.m
+++ External/Server/Planets_Analysis.m
@@ -36,8 +36,28 @@ timecol = 1;
 xcol = 2;
 ycol = 3;
 
+% If the data files are empty, exit
+chest_check = dir(path+"\chest_position.csv");
+if chest_check.bytes == 0
+    score = 0;
+    linegraphLR_X = 0;
+    linegraphLR_Y = 0;
+    BarGraphLR_EL = 0;
+    BarGraphLR_ML = 0;
+    BarGraphLR_C = 0;
+    BarGraphLR_MR = 0;
+    BarGraphLR_ER = 0;
+    linegraphFB_X = 0;
+    linegraphFB_Y = 0;
+    BarGraphFB_EF = 0;
+    BarGraphFB_MF = 0;
+    BarGraphFB_C = 0;
+    BarGraphFB_MB = 0;
+    BarGraphFB_EB = 0;
+    return
+end;
+
 %Time X Y Z
-%chest = readmatrix("head_position.csv"); %For the moment I am using head position, but calling it chest
 chest = readmatrix(path+"\chest_position.csv");
 lfoot = readmatrix(path+"\left_foot_position.csv");
 rfoot = readmatrix(path+"\right_foot_position.csv");
@@ -192,11 +212,7 @@ hBprop = h_back/total_pitch;
 placeholderLR = chest_xy(:,1);
 linegraphLR_X = zeros(size(placeholderLR,2),1);
 
-%for i=1:size(placeholderLR,1)
-%    linegraphLR_X(1,i) = (placeholderLR(i) - deadtime);
-%end
-
-score = 100*midprop + 50*sRprop_v2+50*sLprop_v2 - 100*hRprop_v2-100*hLprop_v2;
+score = 100*midprop + 50*sRprop_v2+50*sLprop_v2;
 linegraphLR_X = (chest_xy(:,1)-deadtime);
 linegraphLR_Y = slant;
 BarGraphLR_EL = hLprop_v2;
@@ -208,13 +224,6 @@ BarGraphLR_ER = hRprop_v2;
 placeholderFB = chest_pyr(:,1);
 linegraphFB_X = zeros(size(placeholderFB,2),1);
 
-%for i=1:size(placeholderFB,1)
-%    linegraphFB_X(1,i) = (placeholderFB(i) - deadtime);
-%end
-
-%linegraphLR_X = ((linegraphLR_X));
-%linegraphFB_X = ((linegraphFB_X));
-
 linegraphFB_X = (chest_pyr(:,1)-deadtime);
 linegraphFB_Y = pitch;
 BarGraphFB_EF = hFprop;
@@ -222,83 +231,4 @@ BarGraphFB_MF = sFprop;
 BarGraphFB_C = midPprop;
 BarGraphFB_MB = sBprop;
 BarGraphFB_EB = hBprop;
-
-
-
-% anglevtime = figure;
-% plot((chest_xy(:,1)-deadtime),slant);
-% hold on
-% grid on
-% %ylim([(-1+hardright_v2) (1+hardleft_v2)])
-% title('Left and Right Lean Over Time')
-% ylabel('Left and Right Lean [degrees]')
-% xlabel('Time [s]')
-% xlim([(chest_xy(1,1)-deadtime) (chest_xy(size(chest_xy,1),1)-deadtime)])
-% ylim([-1*max((1+max(abs(slant))),(hardleft_v2+2)) max((1+max(abs(slant))),(hardleft_v2+2))])
-% 
-% x_values_slant = chest_xy(:,1)-deadtime;
-% SHardLineRight_Y = zeros(size(x_values_slant))+hardright_v2;
-% plot(x_values_slant,SHardLineRight_Y,'Color',[0.6350 0.0780 0.1840])
-% 
-% SHardLineLeft_Y = zeros(size(x_values_slant))+hardleft_v2;
-% plot(x_values_slant,SHardLineLeft_Y,'Color',[0.6350 0.0780 0.1840])
-% 
-% SSoftLineRight_Y = zeros(size(x_values_slant))+softright_v2;
-% plot(x_values_slant,SSoftLineRight_Y,'Color',[0.9290 0.6940 0.1250])
-% 
-% SSoftLineLeft_Y = zeros(size(x_values_slant))+softleft_v2;
-% plot(x_values_slant,SSoftLineLeft_Y,'Color',[0.9290 0.6940 0.1250])
-% 
-% saveas(gcf,'SlantOverTime.png')
-% 
-% hold off
-% 
-% 
-% 
-% bargraph = figure;
-% %bar_x = ["Leaning Left" "Leaning Slightly Left" "Centered" "Leaning Slightly Right" "Leaning Right"];
-% bar_x = categorical({'Extreme Left Lean','Slight Left Lean','Centered','Slight Right Lean','Extreme Right Lean'});
-% bar_x = reordercats(bar_x,{'Extreme Left Lean','Slight Left Lean','Centered','Slight Right Lean','Extreme Right Lean'});
-% bar_y = [hLprop_v2 sLprop_v2 midprop sRprop_v2 hRprop_v2];
-% bar(bar_x,bar_y)
-% title('Time Spent at Severity of Left & Right Lean')
-% ylabel('Percentage of Time Spent')
-% saveas(gcf,'SlantBar.png')
-% 
-% anglevtimePitch = figure;
-% plot((chest_pyr(:,1)-deadtime),pitch);
-% grid on
-% hold on
-% %ylim([(-1+hardback) (1+hardforward)])
-% title('Foward and Backwards Lean Over Time')
-% ylabel('Forward and Backwards Lean [degrees]')
-% xlabel('Time [s]')
-% xlim([(chest_pyr(1,1)-deadtime) (chest_pyr(size(chest_pyr,1))-deadtime)])
-% ylim([-1*max((10+max(abs(pitch),(hardforward+10)))) max((10+max(abs(pitch),(hardforward+10))))])
-% 
-% x_values_pitch = chest_pyr(:,1)-deadtime; %It was working, then I noticed this was xy instead of pyr, I changed it, hopefully it does not break now
-% SHardLineForw_Y = zeros(size(x_values_pitch))+hardforward;
-% plot(x_values_pitch,SHardLineForw_Y,'Color',[0.6350 0.0780 0.1840])
-% 
-% SHardLineBack_Y = zeros(size(x_values_pitch))+hardback;
-% plot(x_values_pitch,SHardLineBack_Y,'Color',[0.6350 0.0780 0.1840])
-% 
-% SSoftLineForw_Y = zeros(size(x_values_pitch))+softforward;
-% plot(x_values_pitch,SSoftLineForw_Y,'Color',[0.9290 0.6940 0.1250])
-% 
-% SSoftLineBack_Y = zeros(size(x_values_pitch))+softback;
-% plot(x_values_pitch,SSoftLineBack_Y,'Color',[0.9290 0.6940 0.1250])
-% 
-% saveas(gcf,'PitchOverTime.png')
-% hold off
-% 
-% bargraphPitch = figure;
-% %bar_x = ["Leaning Left" "Leaning Slightly Left" "Centered" "Leaning Slightly Right" "Leaning Right"];
-% bar_xP = categorical({'Extreme Forward Lean','Slight Forward Lean','Centered','Slight Back Lean','Extreme Back Lean'});
-% bar_xP = reordercats(bar_xP,{'Extreme Forward Lean','Slight Forward Lean','Centered','Slight Back Lean','Extreme Back Lean'});
-% bar_yP = [hFprop sFprop midPprop sBprop hBprop];
-% bar(bar_xP,bar_yP)
-% title('Time Spent at Severity of Forward & Backwards Lean')
-% ylabel('Percentage of Time Spent')
-% saveas(gcf,'PitchBar.png')
 end
