5170bde52b8f3a34913c7785abef9ed1a4ba3757
Spiritdon <109160446+TheBostonGoblin@users.noreply.github.com>
2023-04-17 14:30:13 -0400

    Implimenting new Trees, fixes to player height, gem collection,etc

--- Starchaeologist/Assets/Scripts/RiverScripts/S_GemCollision.cs
+++ Starchaeologist/Assets/Scripts/RiverScripts/S_GemCollision.cs
@@ -2,21 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
-public class S_ObstacleCollider : MonoBehaviour
+public class S_GemCollision : MonoBehaviour
 {
     [SerializeField] Text scoreText;
 
     void OnTriggerEnter(Collider other)
     {
         //when the player hits the obstacle, tell the game manager to deduct points
-        if (other.gameObject.CompareTag("Obstacle"))
+        if (other.gameObject.CompareTag("Tresure"))
         {
 
-            
+
             GameObject.Find("Game Manager").GetComponent<S_RiverGame>().ObstacleHit();
-            scoreScript.Instance.hitScore();
+            scoreScript.Instance.treasureScore();
             Debug.Log(scoreScript.Score);
             Debug.Log("score updated");
             scoreText.text = "Score: " + scoreScript.Score;
